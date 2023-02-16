fac7c06b577c22e473d02b14018c26b7dd1d607d
Elizeu Junior <77870830+ermyjr@users.noreply.github.com>
2023-02-16 17:40:10 -0400

    Add files via upload

---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div `
+`;
+
+export const Image = styled.img `
+
+`;
+
+export const ContainerItems = styled.div `
+
+`;
+
+export const H1 = styled.h1 `
+`;
+
+export const InputLabel = styled.p `
+`;
+
+export const Input = styled.input `
+`;
+
+export const Button = styled.button `
+`;
