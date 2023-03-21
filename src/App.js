ab1843d93b6e0c1b3c3aa8c1c10160d7b5001f31
Ermyjr <ermyjr94@gmail.com>
2023-03-20 20:15:35 -0400

    atualização app

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
 
   async function addNewUser() {
     const data = await axios.post("http://localhost:3001/users", { name: inputName.current.value, age: inputAge.current.value })
+    
     /*   setUsers([...user,
        {id: Math.random(),name: inputName.current.value,age: inputAge.current.value}
       ]);
